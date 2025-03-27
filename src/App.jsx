be87b3d460b53d021f02e53a17ca098a58f57669
N-2023010\user <newyoung124@naver.com>
2025-03-27 13:34:08 +0900

    mb-4

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       </header>
 
       {/* 메인 콘텐츠 - Calendar */}
-      <main className="flex-grow flex items-center justify-center pt-4">
+      <main className="flex-grow flex items-center justify-center mb-4">
         <Calendar />
       </main>
 
